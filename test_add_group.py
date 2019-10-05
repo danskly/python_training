c9e3d207bd604c3e7478c2ff9c683e5646aefa27
D <dan>
2019-10-05 21:29:05 +0300

    Поправлено форматирование в тесте групп

--- test_add_group.py
+++ test_add_group.py
@@ -6,11 +6,11 @@ from application import Application
 
 @pytest.fixture
 def app(request):
-        fixture = Application()
-        request.addfinalizer(fixture.destroy)
-        return fixture
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    
+
 def test_add_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="Hello", header="setter", footer="scooter"))
@@ -21,12 +21,3 @@ def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
-
-
-
-
-    
-
-    
-
-
