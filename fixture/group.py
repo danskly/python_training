256f40007bcef925ceab1aa67cf534ea5acb8420
D <dan>
2019-10-10 01:34:46 +0300

    Сделана общая для всех тестов фикстура в файле conftest.py

--- fixture/group.py
+++ fixture/group.py
@@ -22,8 +22,17 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit grroup creation
+        # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
