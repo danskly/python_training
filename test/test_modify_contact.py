5ea8992774a925d996a3f927a1cefc21555b7212
devoved <danil40@yandex.ru>
2019-10-30 23:56:39 +0300

    Реализована проверка валидности фикстуры и проверка состояния сессии 2

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,6 +3,8 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
+    if app.contact.count == 0:
+        app.contact.create(Contact(firstname='text'))
     app.contact.modify_first_contact(
         Contact(firstname="Leonardo", middlename="", lastname="Davinci", nickname="genius", tittle="innovator",
                 company="ip", address="Florence", home="", mobile="",
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,4 +3,6 @@ from model.group import Group
 
 
 def test_modify_first_group(app):
+    if app.group.count == 0:
+        app.group.create(Group(name='text'))
     app.group.modify_first_group(Group(name="Top", header="Start", footer="Stop"))
